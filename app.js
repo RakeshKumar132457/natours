3c3b74722c8860ac5b1d2c62a5988626beff3962
Roydon Robbins <bruceisbatman4770@gmail.com>
2022-10-10 21:32:28 +0530

    Backend Payment setup

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const cookieParser = require('cookie-parser');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
 const AppError = require('./utils/appError');
@@ -86,6 +87,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 app.all('*', (req, res, next) => {
     next(new AppError(`Can't find ${req.originalUrl}`, 404));
---
+++ controllers/bookingController.js
@@ -0,0 +1,44 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const AppError = require('../utils/appError');
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
+const factory = require('./handlerFactory');
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+    // Get  the currently booked store
+    const tour = await Tour.findById(req.params.tourId);
+
+    // Create checkout session
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        success_url: `${req.protocol}://${req.get('host')}/`,
+        cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+        customer_email: req.user.email,
+        client_reference_id: req.params.tourId,
+        mode: 'payment',
+        line_items: [
+            {
+                price_data: {
+                    unit_amount: tour.price * 100,
+                    currency: 'usd',
+                    product_data: {
+                        name: `${tour.name} tour`,
+                        description: tour.summary,
+                        images: [
+                            `https://www.natours.dev/img/tours/${tour.imageCover}`,
+                        ],
+                    },
+                },
+
+                quantity: 1,
+            },
+        ],
+    });
+
+    // Create session as response
+    res.status(200).json({
+        status: 'success',
+        session,
+    });
+});
