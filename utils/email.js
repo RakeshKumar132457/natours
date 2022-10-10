5551ccb28ecaab5c1ca4076547f7883ac0184628
Roydon Robbins <bruceisbatman4770@gmail.com>
2022-10-10 16:44:42 +0530

    Send email completed

--- utils/email.js
+++ utils/email.js
@@ -1,26 +1,60 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (options) => {
-    // Create a transporter
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD,
-        },
-    });
+module.exports = class Email {
+    constructor(user, url) {
+        this.to = user.email;
+        this.firstName = user.name.split(' ')[0];
+        this.url = url;
+        this.from = `Rakesh Kumar <${process.env.EMAIL_FROM}>`;
+    }
+    newTransport() {
+        if (process.env.NODE_ENV === 'production') {
+            return 1;
+        }
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD,
+            },
+        });
+    }
 
-    // Define Email
-    const mailOptions = {
-        from: 'Rakesh Kumar<rakesh@gmail.com>',
-        to: options.email,
-        subject: options.subject,
-        text: options.message,
-    };
+    async send(template, subject) {
+        // Render HTML template from pug
+        const html = pug.renderFile(
+            `${__dirname}/../views/email/${template}.pug`,
+            {
+                firstName: this.firstName,
+                url: this.url,
+                subject,
+            }
+        );
 
-    // Actually send the email
-    await transporter.sendMail(mailOptions);
+        // Define mail options
+        const mailOptions = {
+            from: this.from,
+            to: this.to,
+            subject,
+            html,
+            text: htmlToText.fromString(html),
+        };
+
+        // Create a transport and send email
+        await this.newTransport().sendMail(mailOptions);
+    }
+
+    async sendWelcome() {
+        await this.send('welcome', 'Welcome to the natours family');
+    }
+
+    async sendPasswordReset() {
+        await this.send(
+            'passwordReset',
+            'Your password reset token (valid for only 10 mins)'
+        );
+    }
 };
-
-module.exports = sendEmail;
